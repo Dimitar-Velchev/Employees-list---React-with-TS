3d7e211b1b706447afae833b4118539d3350e064
Mitko <dimitarvelchev59@gmail.com>
2022-06-20 14:13:25 +0300

    add header

--- employees-list/src/components/Navigation.tsx
+++ employees-list/src/components/Navigation.tsx
@@ -1,22 +1,43 @@
-import {Navbar,Container,Nav} from 'react-bootstrap';
+import { FC } from "react";
+import { Navbar, Container, Nav, Image } from "react-bootstrap";
+import styled from "styled-components";
+import { BsList } from "react-icons/bs";
 
+import logo from "../assets/icon-employee-5.jpg";
 
-const Navigation = () => {
+const Navigation: FC = () => {
   return (
-    <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home">Employees</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <Header bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Image src={logo} height={45} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Hamburger className="d-block">
+          <BsList size={25} />
+        </Hamburger>
+      </Container>
+    </Header>
   );
 };
 
+const Header = styled(Navbar)`
+  padding: 0.5rem 2rem;
+  display: flex;
+  align-items: stretch;
+`;
 
-export default Navigation
+const Hamburger = styled.button`
+  background: none;
+  border: none;
+  padding: 0.75rem;
+  margin-left: 1rem;
+`;
+
+export default Navigation;
